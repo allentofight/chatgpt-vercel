8ed451d8ead930852691325fa15fbdc1d91ca282
ronaldo <kun.xie@qiuweigroup.com>
2023-03-08 15:53:19 +0800

    优化

--- src/default.ts
+++ src/default.ts
@@ -8,4 +8,4 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - 本站仅用于演示，填入自己的 key 才可使用。
-- **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。更多预设请去 [Github](https://github.com/ourongxing/chatgpt-vercel) PR。`
+- **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。`
