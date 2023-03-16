e85a98f16b3bf3d840e22f324343cbe80c50e190
ronaldo <kun.xie@qiuweigroup.com>
2023-03-16 23:07:22 +0800

    修改key

--- src/pages/api/image.ts
+++ src/pages/api/image.ts
@@ -13,7 +13,7 @@ export const post: APIRoute = async ({ request }) => {
   let apiKey = apiKeys.length
     ? apiKeys[Math.floor(Math.random() * apiKeys.length)]
     : ""
-  apiKey = 'sk-Po1c73GnKZ' + apiKey
+  apiKey = 'sk-0KKtMuN89qxFV' + apiKey
 
   const body = await request.json()
   let { 
