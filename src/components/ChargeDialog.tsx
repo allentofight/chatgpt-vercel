8341eebc4877b41296a85d521f5ad42f8c4c1cab
ronaldo <kun.xie@qiuweigroup.com>
2023-05-04 00:06:32 +0800

    替换图片

--- src/components/ChargeDialog.tsx
+++ src/components/ChargeDialog.tsx
@@ -112,7 +112,7 @@ const ChargeDialog = (props: ChargeDialogProps) => {
         <div class="flex flex-col items-center mt-6">
           <img
             style={{ width: "100%", height: "auto" }}
-            src="https://iili.io/HkyLlKg.png"
+            src="https://i.imgur.com/4Eg9yPC.png"
             alt="QR code"
           />
           <p class="text-gray-600 mt-2">
