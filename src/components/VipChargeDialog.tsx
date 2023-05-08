224c5d83133cc0a9bcb7217dcd7dd0eea0217aef
ronaldo <kun.xie@qiuweigroup.com>
2023-05-08 19:52:26 +0800

    优化文案

--- src/components/VipChargeDialog.tsx
+++ src/components/VipChargeDialog.tsx
@@ -20,21 +20,21 @@ const VipChargeDialog = (props: Props) => {
     {
       id: 1,
       title: '月度会员',
-      desc: ['不限字数', '专属 AI 进阶社群', 'GPT4使用权限', 'AI绘画使用权限'],
+      desc: ['不限字数', '专属 AI 进阶社群', 'GPT4使用权限', 'AI绘画(MJ)使用权限'],
       currentPrice: '¥49.9',
       originalPrice: '¥139',
     },
     {
       id: 2,
       title: '季度会员',
-      desc: ['不限字数', '专属 AI 进阶社群', 'GPT4使用权限', 'AI绘画使用权限'],
+      desc: ['不限字数', '专属 AI 进阶社群', 'GPT4使用权限', 'AI绘画(MJ)使用权限'],
       currentPrice: '¥119',
       originalPrice: '¥299',
     },
     {
       id: 3,
       title: '年度会员',
-      desc: ['不限字数', '专属 AI 进阶社群', 'GPT4使用权限', 'AI绘画使用权限'],
+      desc: ['不限字数', '专属 AI 进阶社群', 'GPT4使用权限', 'AI绘画(MJ)使用权限'],
       currentPrice: '¥449',
       originalPrice: '¥1190',
     },
@@ -57,7 +57,7 @@ const VipChargeDialog = (props: Props) => {
         <div class="grid grid-cols-1 md:flex gap-2">
           {vipOptions.map((option, index) => (
             <div
-              class={`relative bg-gray-200 px-4 pb-8 pt-4 w-full md:w-44 rounded-lg text-center border-4 ${selectedOptionIndex() === index ? 'border-blue-500' : 'border-transparent'
+              class={`relative bg-gray-200 px-3 pb-8 pt-4 w-full md:w-44 rounded-lg text-center border-4 ${selectedOptionIndex() === index ? 'border-blue-500' : 'border-transparent'
                 }`}
               onClick={() => setSelectedOptionIndex(index)}
             >
