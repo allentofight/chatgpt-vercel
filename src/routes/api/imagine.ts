cb1abb7235b565468bf98d8f110b7bb221e0ac52
ronaldo <kun.xie@qiuweigroup.com>
2023-06-03 16:05:16 +0800

    优化

--- src/routes/api/imagine.ts
+++ src/routes/api/imagine.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { APIEvent } from "solid-start/api"
+// import type { APIEvent } from "solid-start/api"
 import { Midjourney } from "midjourney";
 import { ResponseError } from "../../interfaces";
 export const config = {
   runtime: "edge",
 };
 
-export async function POST({ request }: APIEvent) {
+export async function POST({ request }) {
   const { prompt } = await request.json();
+  console.log('prompt = ', prompt)
   console.log("imagine.handler", prompt);
   const client = new Midjourney({
     ServerId: '1112940597846749225',
