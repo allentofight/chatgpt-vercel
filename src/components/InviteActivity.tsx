57d35257f1180e1259ecd41ea81943728296c7e1
ronaldo <kun.xie@qiuweigroup.com>
2023-08-09 09:47:06 +0800

    修改删除按钮大小

--- src/components/InviteActivity.tsx
+++ src/components/InviteActivity.tsx
@@ -33,7 +33,7 @@ export default function InviteActivity() {
             </style>
             <Show when={showDialog()}>
                 <div class="flex flex-col items-end fixed bottom-48 right-3 hidden">
-                    <img src="https://b1.beisheng.com/common/starchain_self_image/2308/08/close.png" onClick={() => {
+                    <img class="w-6 h-6" src="https://b1.beisheng.com/common/starchain_self_image/2308/08/close.png" onClick={() => {
                         closeDialog()
                     }} />
                     <img class="mt-2 activity rounded" src="https://b1.beisheng.com/common/starchain_self_image/2308/08/img_v2_2af28970-a500-40f3-b116-b2bf4b5dee0g.jpg"
