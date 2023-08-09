9597560019cceb18271d03e91c12ca769a133bdb
ronaldo <kun.xie@qiuweigroup.com>
2023-08-09 09:54:22 +0800

    优化

--- src/components/InviteActivity.tsx
+++ src/components/InviteActivity.tsx
@@ -24,19 +24,12 @@ export default function InviteActivity() {
 
     return (
         <>
-            <style>
-                {`
-                    .activity {
-                        width: 150px;
-                    }
-                `}
-            </style>
             <Show when={showDialog()}>
                 <div class="flex flex-col items-end fixed bottom-48 right-3 hidden">
                     <img class="w-6 h-6" src="https://b1.beisheng.com/common/starchain_self_image/2308/08/close.png" onClick={() => {
                         closeDialog()
                     }} />
-                    <img class="mt-2 activity rounded" src="https://b1.beisheng.com/common/starchain_self_image/2308/08/img_v2_2af28970-a500-40f3-b116-b2bf4b5dee0g.jpg"
+                    <img class="mt-2 w-[112px] sm:w-[150px] rounded" src="https://b1.beisheng.com/common/starchain_self_image/2308/08/img_v2_2af28970-a500-40f3-b116-b2bf4b5dee0g.jpg"
                         onClick={() => {
                             setShowInviteDialog(true)
                         }} />
