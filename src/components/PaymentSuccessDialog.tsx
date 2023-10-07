724d7ee57664abe5d1e2152a3cc7eb93fcd88d5d
ronaldo <kun.xie@qiuweigroup.com>
2023-10-07 17:41:40 +0800

    优化文案

--- src/components/PaymentSuccessDialog.tsx
+++ src/components/PaymentSuccessDialog.tsx
@@ -3,7 +3,7 @@ export default function PaymentSuccessDialog() {
     <div class='fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center'>
       <div class="bg-white p-8 rounded-lg flex flex-col items-center">
         <h2 class="text-2xl mb-6">恭喜你，充值成功!</h2>
-        <p class="mb-4">欢迎扫码添加坤哥微信进GPT技巧群</p>
+        <p class="mb-4">欢迎扫码添加坤哥微信进GPT变现&技巧群</p>
         <img class="mb-6" src="https://i.imgtg.com/2023/05/04/CkYCM.png" alt="QR Code" width="432" height="496" />
         <button class="bg-red-500 text-white rounded px-4 py-2" onClick={() => window.location.href = '/'}>
           返回首页
