d4461bb4a6669c863ef8eeb0cb1676fb910b15ab
ronaldo <kun.xie@qiuweigroup.com>
2023-12-26 13:33:02 +0800

    移除无用代码

--- src/routes/payment.tsx
+++ src/routes/payment.tsx
@@ -1,11 +1,6 @@
 import PaymentComponent from "~/components/PaymentComponent"
-import Layout from "~/layout"
-
-import { parseMjPrompts } from "~/utils"
 
 export default function () {
-
-  const prompts = parseMjPrompts()
 
   return (
     <PaymentComponent />
