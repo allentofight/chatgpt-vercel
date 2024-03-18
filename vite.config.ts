4c6421b5522da122c3f4bd40f59bb74e0dc40e82
ronaldo <kun.xie@qiuweigroup.com>
2024-03-18 21:46:08 +0800

    优化

--- vite.config.ts
+++ vite.config.ts
@@ -52,11 +52,11 @@ export default defineConfig({
       }
     }),
     legacy({
-        targets: ['defaults', 'not IE 11', 'chrome 79', 'maintained node versions'],
-        additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-        // 根据你自己需要导入相应的polyfill:  https://github.com/vitejs/vite/tree/main/packages/plugin-legacy#polyfill-specifiers
-        modernPolyfills: ['es.promise.finally', 'es/array', 'es/map', 'es/set'],
-    }),
+      targets: ['defaults', 'not IE 11', 'chrome 79', 'maintained node versions'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+      // 根据你自己需要导入相应的polyfill:  https://github.com/vitejs/vite/tree/main/packages/plugin-legacy#polyfill-specifiers
+      modernPolyfills: ['es.promise.finally', 'es/array', 'es/map', 'es/set', 'web.queue-microtask'],
+  }),
     solid({ ssr: false, adapter: adapter() }),
     VitePWA({
       base: "/",
